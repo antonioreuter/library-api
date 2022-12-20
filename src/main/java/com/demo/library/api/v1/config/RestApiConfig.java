70d7f9588c6a69c9b023ff7dd7e5fe36ac6242e2
Antonio Reuter <90752955+antonioreuterNL@users.noreply.github.com>
2022-12-20 16:56:44 +0100

    Added the configuration to instantiate the OpenAPI Validator filter and interceptor

--- src/main/java/com/demo/library/api/v1/config/RestApiConfig.java
+++ src/main/java/com/demo/library/api/v1/config/RestApiConfig.java
@@ -1,2 +1,50 @@
-package com.demo.library.api.v1.config;public class RestApiConfig {
+package com.demo.library.api.v1.config;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.report.JsonValidationReportFormat;
+import com.atlassian.oai.validator.report.SimpleValidationReportFormat;
+import com.atlassian.oai.validator.springmvc.*;
+import com.demo.library.storage.database.jpa.BookRepositoryJPA;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+@Configuration
+public class RestApiConfig implements WebMvcConfigurer{
+	@Bean
+	public Filter validationFilter() {
+		var filter = new OpenApiValidationFilter(true, false);
+
+		return filter;
+	}
+
+
+
+	@Bean
+	public WebMvcConfigurer getOpenApiValidationInterceptor(@Value("schemas/api-v1.yaml") final String apiSpecUrl) {
+		final OpenApiInteractionValidator validator = OpenApiInteractionValidator
+				.createFor(apiSpecUrl)
+				.withLevelResolver(SpringMVCLevelResolverFactory.create())
+				.build();
+		final OpenApiValidationInterceptor openApiValidationInterceptor = new OpenApiValidationInterceptor(validator);
+
+
+		return new WebMvcConfigurer() {
+			@Override
+			public void addInterceptors(final InterceptorRegistry registry) {
+				registry.addInterceptor(openApiValidationInterceptor);
+			}
+		};
+	}
+
+
 }
